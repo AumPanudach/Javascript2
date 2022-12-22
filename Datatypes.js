ba081770b959b5202ead8f10bbcb659e2108bba4
AumPanudach <6406021611026@fitm.kmutnb.ac.th>
2022-12-22 08:28:03 +0700

    update

---
+++ Datatypes.js
@@ -0,0 +1,16 @@
+const peopl = ["Aaron","Mel","John"];
+const one = 1;
+const str = "Hollo World";
+const b = true;
+const employee = {
+    firstname : "Anirach",
+    lastname : "Mingkhwan",
+};
+function sayHello(person){
+    console.log("Hello" + person.firstname);
+}
+
+console.log(typeof peopl);
+console.log(typeof sayHello);
+console.log(employee instanceof Array);
+sayHello(employee);
---
+++ DisplayString.js
@@ -0,0 +1,5 @@
+const message = "Hello";
+const place = "World";
+console.log('Hellow,World');
+console.log('Hello, %s!',place);
+console.log(`${message},${place}!`);
